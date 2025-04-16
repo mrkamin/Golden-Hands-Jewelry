ad9c4e3d5b682c24ec1862424a42d9526ad17319
Rafi <mrkamin2@gmail.com>
2025-04-16 15:49:30 +0500

    Build navbar and footer component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="p-10 text-center">
-      <h1 className="text-4xl font-bold text-pink-700">Golden Hands</h1>
+      <h1 className="text-4xl font-bold text-pink-700">Golden Hands Jewelry</h1>
       <p className="mt-4 text-xl">Handmade Jewelry Crafted with Love & Meaning</p>
     </main>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <h1>Footer</h1>
-    </div>
-  )
+    <footer className="bg-[#1f1f38] text-white py-8 mt-10">
+      <div className="max-w-7xl mx-auto px-4 text-center">
+        <p className="text-lg font-semibold">Golden Hands Jewelry</p>
+        <p className="text-sm mt-2">Handmade with love ✨ | Est. 2025</p>
+        <p className="text-xs mt-4 text-gray-400">
+        © {new Date().getFullYear()} Golden Hands. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
